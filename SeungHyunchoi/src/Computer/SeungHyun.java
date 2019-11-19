2e3d0210366e9a745e2def2c19a30dbd48e2e2a1
choisung hyun <chltmdgus99@naver.com>
2019-11-19 21:50:57 +0900

    new println ChoiSeungHyun

--- SeungHyunchoi/src/Computer/SeungHyun.java
+++ SeungHyunchoi/src/Computer/SeungHyun.java
@@ -4,5 +4,6 @@ public class SeungHyun {
 	public static void main(String[] args) {
 		System.out.println("ComputerSW");
 		System.out.println("wonkwang Univ");
+		System.out.println("ChoiSeungHyun");
 	}
 }
